6a59866821b69c2e255e851140d0c0207bd031c0
r0bert <robertdsgabriel@gmail.com>
2024-10-30 08:58:15 -0300

    up

--- dados/data/query.sql
+++ dados/data/query.sql
@@ -1,9 +1,6 @@
-SELECT *
-       
-FROM customers AS t1
-
-LEFT JOIN transactions AS t2
-ON t1.idCustomer = t2.idCustomer
-
-LEFT JOIN transactions_product as t3
-ON t2.idTransaction = t3.idTransaction
+SELECT 
+dtRef,
+COUNT(*)
+FROM fs_general
+GROUP BY dtRef
+ORDER BY 1 DESC
--- src/feature-store/fs_general.sql
+++ src/feature-store/fs_general.sql
@@ -1,66 +1,53 @@
 WITH tb_rfv AS (
-    SELECT
 
+    SELECT 
         idCustomer,
 
+        CAST(min(julianday('{date}') - julianday(dtTransaction))
+            AS INTEGER) + 1 AS recenciaDias,
 
-CAST (min(julianday('now') -  julianday(dtTransaction)) AS INTEGER)  AS recenciaDias,   
+        COUNT(DISTINCT DATE(dtTransaction)) AS frequenciaDias,
 
-COUNT(DISTINCT DATE(dtTransaction)) AS frequenciaDias,
+        SUM(CASE
+                WHEN pointsTransaction > 0 THEN pointsTransaction
+            END) AS valorPoints
 
+    FROM transactions
 
-SUM(CASE WHEN PointsTransaction > 0 THEN PointsTransaction 
-END) AS pointsAcumulados
+    WHERE dtTransaction < '{date}'
+    AND dtTransaction >= DATE('{date}', '-21 day')
 
-FROM transactions
-
-WHERE dtTransaction < '{date}'
-AND dtTransaction >= DATE('{date}', '-21 day')
-GROUP BY idCustomer
-
-
-
+    GROUP BY idCustomer
 ),
 
 tb_idade AS (
-        SELECT
-            t1.idCustomer,
-            CAST(MAX(julianday('{date}') - julianday(t2.dtTransaction))
-                AS INTEGER) AS idadeBaseDias
 
-        FROM tb_rfv AS t1
+    SELECT
 
-        LEFT JOIN transactions AS t2
+        t1.idCustomer,
 
-        ON t1.idCustomer = t2.idCustomer
+        CAST(MAX(julianday('{date}') - julianday(t2.dtTransaction))
+                AS INTEGER) + 1 AS idadeBaseDias
 
-        LEFT JOIN customers AS t3
+    FROM tb_rfv AS t1
 
-        ON t1.idCustomer = t3.idCustomer
+    LEFT JOIN transactions AS t2
+    ON t1.idCustomer = t2.idCustomer
 
-
-
-        GROUP BY t2.idCustomer 
-
+    GROUP BY t2.idCustomer
 
 )
 
-/* JUNTANDO TUDO */
-
-SELECT 
-        '{date}' AS dtRef,
-        t1.*,
-         t2.idadeBaseDias,
-         t3.flEmail
-
+SELECT
+     '{date}' AS dtRef,
+     t1.*,
+     t2.idadeBaseDias,
+     t3.flEmail
 
 FROM tb_rfv AS t1
 
 LEFT JOIN tb_idade AS t2
 ON t1.idCustomer = t2.idCustomer
 
-/* EMAIL VINCULADO */
- LEFT JOIN customers AS t3
- ON t1.idCustomer = t3.idCustomer
-
-/* ctrl+h em cima da data para poder "andar no tempo" */
+LEFT JOIN customers AS t3
+ON t1.idCustomer = t3.idCustomer
